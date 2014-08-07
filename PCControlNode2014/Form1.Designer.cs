182eed9d58699fc44e4ecccdecf156a451c14c4b
Jian JIAO <Jian.Jiao5800@gmail.com>
2014-08-07 18:54:32 +0800

    向内网发送发送数据成功

--- PCControlNode2014/Form1.Designer.cs
+++ PCControlNode2014/Form1.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnSendTest = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSendTest
+            // 
+            this.btnSendTest.Location = new System.Drawing.Point(45, 22);
+            this.btnSendTest.Name = "btnSendTest";
+            this.btnSendTest.Size = new System.Drawing.Size(75, 23);
+            this.btnSendTest.TabIndex = 0;
+            this.btnSendTest.Text = "发送测试";
+            this.btnSendTest.UseVisualStyleBackColor = true;
+            this.btnSendTest.Click += new System.EventHandler(this.btnSendTest_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnSendTest);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSendTest;
     }
 }
 
--- PCControlNode2014/Form1.cs
+++ PCControlNode2014/Form1.cs
@@ -7,14 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
 
 namespace PCControlNode2014
 {
     public partial class Form1 : Form
     {
+        System.Net.Sockets.UdpClient udpClient1;
         public Form1()
         {
+            udpClient1 = new System.Net.Sockets.UdpClient();
             InitializeComponent();
+        }
+
+        private void btnSendTest_Click(object sender, EventArgs e)
+        {
+            byte[] testBytes = { 0x11, 0x22 };
+            udpClient1.Send(testBytes, testBytes.Length, new System.Net.IPEndPoint(IPAddress.Parse("192.168.7.255"), 4567));
         }
     }
 }
